82b17060652762615d47f53a0437bede8ad59381
Diego Azulay <dsazulay@gmail.com>
2017-12-29 16:48:50 -0400

    Add graph class

---
+++ graph/graph.hpp
@@ -0,0 +1,42 @@
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+template <typename T>
+class Graph
+{
+public:
+    typedef T node;
+    Graph();
+    ~Graph();
+    vector<node> neighbors(node);
+
+private:
+    unordered_map<node, vector<node>> edges;
+};
+
+
+template <typename T>
+Graph<T>::Graph()
+{
+    edges = {
+        {'A', {'B'}},
+        {'B', {'A', 'C', 'D'}},
+        {'C', {'A'}},
+        {'D', {'E', 'A'}},
+        {'E', {'B'}},
+    };
+}
+
+template <typename T>
+Graph<T>::~Graph()
+{
+
+}
+
+template <typename T>
+vector<T> Graph<T>::neighbors(T id)
+{
+    return edges[id];
+}
