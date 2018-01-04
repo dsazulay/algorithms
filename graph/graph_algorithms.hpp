8d5888c95af5eb37f212ec778528016403c3b155
Diego Azulay <dsazulay@gmail.com>
2018-01-04 18:27:18 -0400

    Add algorithm for detect cycle in graph

--- graph/graph_algorithms.hpp
+++ graph/graph_algorithms.hpp
@@ -61,3 +61,46 @@ void deepFirsthSearch(Graph graph, typename Graph::node start)
         }
     }
 }
+
+template <typename Graph>
+bool isCyclicUtil(Graph graph, typename Graph::node node, unordered_set<typename Graph::node> visited, unordered_set<typename Graph::node> recStack)
+{
+    if (!visited.count(node))
+    {
+        visited.insert(node);
+        recStack.insert(node);
+    
+        auto edges = graph.neighbors(node);
+
+        for (auto it = edges.begin(); it != edges.end(); ++it)
+        {
+            if (!visited.count(*it) && isCyclicUtil(graph, *it, visited, recStack))
+                return true;
+            else if (recStack.count(*it))
+                return true;
+        }
+    }
+
+    recStack.erase(node);
+    return false;
+}
+
+template <typename Graph>
+bool isCyclic(Graph graph, typename Graph::node start)
+{
+    typedef typename Graph::node node;
+    
+    unordered_set<node> visited;
+    unordered_set<node> recStack;
+
+    auto edges = graph.neighbors(start);
+
+    // TODO: iterate over all vertices
+    // for (auto it = edges.begin(); it != edges.end(); ++it)
+    // {
+        if (isCyclicUtil(graph, start, visited, recStack))
+            return true;
+    // }
+
+    return false;
+}
--- graph/main.cpp
+++ graph/main.cpp
@@ -4,7 +4,10 @@ int main (int argc, char *argv[])
 {
     Graph<char> myGraph;
 
-    deepFirsthSearch(myGraph, 'A');
+    if (isCyclic(myGraph, 'A'))
+        cout << "true" << endl;
+    else
+        cout << "false" << endl;
 
     return 0;
 }
